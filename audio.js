fe12e8c50bbd9742df67718189c05ccac6e946f4
narekmkhoyan1-sketch <narek.mkhoyan1@tumo.org>
2026-07-11 13:29:54 +0400

    first commit

---
+++ audio.js
@@ -0,0 +1,28 @@
+var data = {
+    title: [
+        "Alexandra Capitanescu - Choke Me",
+        "Dara - Bangaranga",
+        "Essyla - Dancing On The Ice",
+        "Justyna Steczkowska - GAJA",
+        "Sona Umroyan - Tsaghikneri Ashxarhum",
+        "Tamara Zivkovic - Nova Zora"     
+   ],
+   song: [
+    "music/Alexandra Capitanescu - Choke Me.mp3",
+    "music/Dara - Bangaranga.mp3",
+    "music/Essyla - Dancing On The Ice.mp3",
+    "music/Justyna Steczkowska - GAJA.mp3",
+    "music/Sona Umroyan - Tsaghikneri Ashxarhum.mp3",
+    "music/Tamara Zivkovic - Nova Zora.mp3"
+    
+   ],
+
+   poster: [
+    "cover/cover-1.jpg",
+    "cover/cover-2.jpg",
+    "cover/cover-3.jpg",
+    "cover/cover-4.jpg",
+    "cover/cover-5.webp",
+    "cover/cover-6.jpg"
+   ]
+}
